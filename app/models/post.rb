2b28417dfde7f4b240597df42df2e994d34a5cae
manpreet <manpreet.dhaliwal22@gmail.com>
2021-02-02 15:20:06 -0800

    styling added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post < ApplicationRecord
+    before_save :capitalize_title
     has_many :comments, dependent: :destroy 
     validates :title, presence: {message: 'must be provided'},uniqueness: true
-    # validates :title, uniqueness: true
+   validates :title, uniqueness: true
     validates :body , length:{minimum: 50}
+    private
+    def capitalize_title
+        self.title.capitalize!
+    end
 end
